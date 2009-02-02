b796f3d13437ecfb8ac05a7a95963e3d51f51ce8
mattc <winescout@gmail.com>
2009-02-01 23:55:51 -0600

    depedencies handled

--- config/dependencies.rb
+++ config/dependencies.rb
@@ -1,6 +1,6 @@
 # dependencies are generated using a strict version, don't forget to edit the dependency versions when upgrading.
-merb_gems_version = "0.9.10"
-dm_gems_version   = "0.9.6"
+merb_gems_version = " >=1.0.8.1"
+dm_gems_version   = ">=0.9.6"
 
 # For more information about each component, please read http://wiki.merbivore.com/faqs/merb_components
 dependency "merb-action-args", merb_gems_version
@@ -15,6 +15,7 @@ dependency "merb-auth-slice-password", merb_gems_version
 dependency "merb-param-protection", merb_gems_version
 dependency "merb-exceptions", merb_gems_version
  
+dependency "do_mysql", dm_gems_version
 dependency "dm-core", dm_gems_version         
 dependency "dm-aggregates", dm_gems_version   
 dependency "dm-migrations", dm_gems_version   
